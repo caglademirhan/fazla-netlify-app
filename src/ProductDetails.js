550e44f96a7fdbd5d20b6f1620dd2378706d57e4
Çağla Demirhan <demirhan18itu@gmail.com>
2023-09-24 21:49:02 +0300

    add/remove favorites in detail page

--- src/ProductDetails.js
+++ src/ProductDetails.js
@@ -1,25 +1,25 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
+import { FaHeart, FaShoppingCart } from 'react-icons/fa';
 import { useECommerce } from './ECommerceContext';
 
 function ProductDetails() {
-  const { id } = useParams(); // Get the 'id' parameter from the URL
+  const { id } = useParams();
   const [product, setProduct] = useState(null);
   const [quantity, setQuantity] = useState(1);
-
-  const { addToCart } = useECommerce();
+  const { addToCart, addToFavorites, removeFromFavorites, state } = useECommerce(); // Add this line
 
   useEffect(() => {
-    // Fetch the product details based on the 'id' parameter
-    axios.get(`https://dummyjson.com/products/${id}`) // Update the URL as needed
+    axios
+      .get(`https://dummyjson.com/products/${id}`)
       .then((response) => {
-        setProduct(response.data); // Set the product details from the response
+        setProduct(response.data);
       })
       .catch((error) => {
         console.error('Error fetching product details:', error);
       });
-  }, [id]); // Trigger the fetch when the 'id' parameter changes
+  }, [id]);
 
   const increaseQuantity = () => {
     setQuantity(quantity + 1);
@@ -32,23 +32,59 @@ function ProductDetails() {
   };
 
   const handleAddToCart = () => {
-    const totalPrice = product.price * quantity; // Calculate the total price
-    addToCart({ ...product, quantity, totalPrice }); // Add to cart with the selected quantity and total price
+    addToCart({ ...product, quantity }); // Use the selected quantity
+  };
+
+  const handleToggleFavorite = () => {
+    const isFavorite = state.favorites.some((item) => item.id === product.id);
+
+    if (isFavorite) {
+      removeFromFavorites(product);
+    } else {
+      addToFavorites(product);
+    }
   };
 
   if (!product) {
-    return null; // Return null if product is still loading
+    return null;
   }
 
+  const isFavorite = state.favorites.some((item) => item.id === product.id);
+
   return (
-    <div className="product-details" style={{ textAlign: 'center', padding: '20px' }}>
+    <div
+      className="product-details"
+      style={{
+        width: '70%', // Set a fixed width
+        margin: '0 auto', // Center the div horizontally
+        padding: '20px',
+        border: '1px solid #ccc',
+        borderRadius: '8px',
+        boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
+        textAlign: 'center',
+        position: 'relative', // Add position relative to the div
+      }}
+    >
+      {/* Move the heart button */}
+      <FaHeart
+        onClick={handleToggleFavorite}
+        style={{
+          position: 'absolute', // Position the heart button absolutely
+          top: '10px', // Adjust top position as needed
+          right: '10px', // Adjust right position as needed
+          color: isFavorite ? 'red' : 'gray',
+          cursor: 'pointer',
+          fontSize: '24px', // Increase the font size
+        }}
+      />
+      <span style={{ fontSize: '18px', position: 'absolute', top: '10px', right: '40px' }}>
+        Add to Favorites
+      </span>
       <h1>{product.title}</h1>
       <img src={product.images[0]} alt={product.title} style={{ width: '300px', height: '300px', objectFit: 'cover' }} />
       <p>{product.description}</p>
-      <p className="price" style={{ fontWeight: 'bold', color: '#007bff' }}>
-        ${product.price} x {quantity} = ${(product.price * quantity).toFixed(2)} {/* Display total price */}
-      </p>
-      <div className="quantity-controls">
+      <div className="quantity-controls" style={{ marginTop: '20px' }}>
+        {/* Add increase and decrease buttons here */}
         <button
           onClick={decreaseQuantity}
           style={{
@@ -79,6 +115,9 @@ function ProductDetails() {
           +
         </button>
       </div>
+      <p className="price" style={{ fontWeight: 'bold', color: '#007bff' }}>
+        ${product.price * quantity} {/* Update the price */}
+      </p>
       <button
         onClick={handleAddToCart}
         style={{
@@ -96,9 +135,14 @@ function ProductDetails() {
       </button>
     </div>
   );
+  
 }
 
 export default ProductDetails;
+
+
+
+
 
 
 
